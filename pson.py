e3396f78512a917fad39439a73f885fa2db822ea
shadowlegend551 <74644203+shadowlegend551@users.noreply.github.com>
2023-06-05 18:23:57 +0300

    Create pson.py

---
+++ pson.py
@@ -0,0 +1,25 @@
+from json import load, dump
+
+
+def load_json(filename):
+    try:
+        with open(filename) as file:
+            return load(file)
+    except FileNotFoundError:
+        print(f'File not saved: No file called "{filename}".')
+
+
+def save_json(filename, dictionary):
+    try:
+        with open(filename, 'w') as file:
+            dump(dictionary, file, indent=4)
+    except FileNotFoundError:
+        print(f'File not saved: No file called "{filename}".')
+
+
+def save_json_formatted(filename, dictionary):
+    try:
+        with open(filename, 'w') as file:
+            dump(dictionary, file, indent=4)
+    except FileNotFoundError:
+        print(f'File not saved: No file called "{filename}".')
