6569761d8f987dab40dfbf86f91fb7d98a9e5a27
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2023-09-06 21:08:44 +0300

    minor refactoring

--- pson.py
+++ pson.py
@@ -6,13 +6,13 @@ def load_json(filename):
         with open(filename) as file:
             return load(file)
     except FileNotFoundError:
-        print(f'File not saved: No file called "{filename}".')
+        print(f'File not loaded: No file called "{filename}".')
 
 
 def save_json(filename, dictionary):
     try:
         with open(filename, 'w') as file:
-            dump(dictionary, file, indent=4)
+            dump(dictionary, file)
     except FileNotFoundError:
         print(f'File not saved: No file called "{filename}".')
 
